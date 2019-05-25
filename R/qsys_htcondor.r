33f120e6be10edc7c48ae3e16191f62cd7065651
Brian O'Meara <omeara.brian@gmail.com>
2019-05-25 01:36:40 -0400

    updating with documentation and correct names

--- R/qsys_htcondor.r
+++ R/qsys_htcondor.r
@@ -3,6 +3,14 @@
 #' Derives from QSys to provide HTCondor-specific functions
 #'
 #' @keywords internal
+#' @examples
+#' \dontrun{
+#' options(clustermq.scheduler="HTCONDOR")
+#' library(clustermq)
+#' library(foreach)
+#' register_dopar_cmq(n_jobs=2, memory=1024) # accepts same arguments as `workers`
+#' foreach(i=1:3) %dopar% sqrt(i) # this will be executed as jobs
+#' }
 HTCONDOR = R6::R6Class("HTCONDOR",
     inherit = QSys,
 
@@ -16,7 +24,7 @@ HTCONDOR = R6::R6Class("HTCONDOR",
             private$job_id = opts$job_name
             filled = private$fill_template(opts)
 s
-            success = system("condor.submit", input=filled, ignore.stdout=TRUE)
+            success = system("condor_submit", input=filled, ignore.stdout=TRUE)
             if (success != 0) {
                 print(filled)
                 stop("Job submission failed with error code ", success)
@@ -25,7 +33,7 @@ s
 
         finalize = function(quiet=self$workers_running == 0) {
             if (!private$is_cleaned_up) {
-                system(paste("condor.rm ", private$job_id),
+                system(paste("condor_rm ", private$job_id),
                        ignore.stdout=quiet, ignore.stderr=quiet, wait=FALSE)
                 private$is_cleaned_up = TRUE
             }
